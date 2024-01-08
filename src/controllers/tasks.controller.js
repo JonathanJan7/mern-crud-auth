04fb732d7acc9f4606688fce2c092491cc6db4f5
JonathanJan7 <jonathan.jan@est.fi.uncoma.edu.ar>
2024-01-08 15:14:02 -0300

    task controller & task model

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,15 +1,28 @@
 import Task  from "../models/task.model.js";
 
 export const getTasks = async(req, res) => {
-    const tasks = await Task.find();
+    const tasks = await Task.find({user: req.user.id}).populate('user');
     res.json(tasks);
 };
-export const getTask = async (req, res) => { };
+export const getTask = async (req, res) => {
+    const task = await Task.findById(req.params.id).populate('user');
+    if (!task) returnres.status(404).json({ message: "Task not found" });
+    res.json(task);
+ };
 export const createTask = async (req, res) => {
     const {title, description, date} = req.body;
-    const newTask = new Task({title, description, date});
+    //Nueva tarea con usuario asignado que viene el middelware autenticado
+    const newTask = new Task({title, description, date, user: req.user.id});
     const savedTask = await newTask.save();
     res.json(savedTask);
  };
-export const updateTask = async (req, res) => { };
-export const deleteTask = async (req, res) => { };
+export const updateTask = async (req, res) => {
+    const task = await Task.findByIdAndUpdate(req.params.id);
+    if(!task) return res.status(404).json({message: "Task not found"});
+    res.json(task);
+ };
+export const deleteTask = async (req, res) => { 
+    const task = await Task.findByIdAndDelete(req.params.id, req.body, {new: true});
+    if (!task) return res.status(404).json({ message: "Task not found" });
+    return res.sendStatus(204);
+};
